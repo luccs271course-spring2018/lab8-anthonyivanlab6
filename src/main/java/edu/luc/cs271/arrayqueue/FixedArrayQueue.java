11566f5c6646afffc3182945ba701cc8c733f686
Ivan Kavanagh <ikavanagh@luc.edu>
2017-10-25 20:58:01 +0000

    initial push lab 8

--- src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
+++ src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
-  private final int capacity;
+  private int capacity;
 
   private int size;
 
@@ -14,7 +14,7 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   private int rear;
 
-  private final E[] data;
+  private E[] data;
 
   // TODO why do we need an explicit constructor?
 
@@ -30,35 +30,83 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
   @Override
   public boolean offer(final E obj) {
     // TODO
-    return false;
-  }
+   // return false;
+   if(size == capacity)
+   {
+     reallocate();
+   }
+   size++; 
+   rear = (rear + 1) % capacity; 
+   data[rear] = obj;
+  return true;
+ }
 
   @Override
   public E peek() {
     // TODO
+    if(size == 0){
     return null;
+    }
+    else
+    {
+      return data[front];
+    }
   }
 
   @Override
   public E poll() {
     // TODO
+   if(size == 0){
     return null;
+    }
+    else
+    {
+      E result = data[front];
+      front = (front + 1) % capacity;
+      size--;
+      return result;
+    }
+   // return null;
   }
 
   @Override
   public boolean isEmpty() {
     // TODO
-    return true;
+    return(size() == 0);
   }
 
   @Override
   public int size() {
     return size;
   }
+  @SuppressWarnings("unchecked")  private void reallocate() {  
+  int newCapacity = 2 * capacity;  
+  E[] newData = (E[]) new Object[newCapacity];
+  int j = front; 
+  for (int i = 0; i < size; i++){ 
+    newData[i] = data[j];
+    j = (j + 1) % capacity; 
+  } 
+  front = 0;  
+  rear = size - 1; 
+  capacity = newCapacity; 
+  data = newData; 
+  }
+
+
+
 
   @Override
   public List<E> asList() {
     // TODO implement using an ArrayList preallocated with the right size
-    return Arrays.asList();
+    
+     List<E> result = new ArrayList<E>();
+   while(isEmpty() == false){
+     result.add(poll());
+   }
+   return result;
+    
   }
 }
+  
+
