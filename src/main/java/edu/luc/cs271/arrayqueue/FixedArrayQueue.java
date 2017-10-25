1f10a051ff4dfd4a8fe15f16622077970fb31337
Anthony LiFonti <alifonti@luc.edu>
2017-10-25 21:45:51 +0000

    Single Queue and Answers

--- src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
+++ src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -1,7 +1,6 @@
 package edu.luc.cs271.arrayqueue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class FixedArrayQueue<E> implements SimpleQueue<E> {
@@ -30,25 +29,22 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
   @Override
   public boolean offer(final E obj) {
     // TODO
-   // return false;
-   if(size == capacity)
-   {
-     reallocate();
-   }
-   size++; 
-   rear = (rear + 1) % capacity; 
-   data[rear] = obj;
-  return true;
- }
+    // return false;
+    if (size == capacity) {
+      return false;
+    }
+    size++;
+    rear = (rear + 1) % capacity;
+    data[rear] = obj;
+    return true;
+  }
 
   @Override
   public E peek() {
     // TODO
-    if(size == 0){
-    return null;
-    }
-    else
-    {
+    if (size == 0) {
+      return null;
+    } else {
       return data[front];
     }
   }
@@ -56,57 +52,51 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
   @Override
   public E poll() {
     // TODO
-   if(size == 0){
-    return null;
-    }
-    else
-    {
+    if (size == 0) {
+      return null;
+    } else {
       E result = data[front];
       front = (front + 1) % capacity;
       size--;
       return result;
     }
-   // return null;
+    // return null;
   }
 
   @Override
   public boolean isEmpty() {
     // TODO
-    return(size() == 0);
+    return (size() == 0);
   }
 
   @Override
   public int size() {
     return size;
   }
-  @SuppressWarnings("unchecked")  private void reallocate() {  
-  int newCapacity = 2 * capacity;  
-  E[] newData = (E[]) new Object[newCapacity];
-  int j = front; 
-  for (int i = 0; i < size; i++){ 
-    newData[i] = data[j];
-    j = (j + 1) % capacity; 
-  } 
-  front = 0;  
-  rear = size - 1; 
-  capacity = newCapacity; 
-  data = newData; 
+
+  @SuppressWarnings("unchecked")
+  private void reallocate() {
+    int newCapacity = 2 * capacity;
+    E[] newData = (E[]) new Object[newCapacity];
+    int j = front;
+    for (int i = 0; i < size; i++) {
+      newData[i] = data[j];
+      j = (j + 1) % capacity;
+    }
+    front = 0;
+    rear = size - 1;
+    capacity = newCapacity;
+    data = newData;
   }
-
-
-
 
   @Override
   public List<E> asList() {
     // TODO implement using an ArrayList preallocated with the right size
-    
-     List<E> result = new ArrayList<E>();
-   while(isEmpty() == false){
-     result.add(poll());
-   }
-   return result;
-    
+
+    List<E> result = new ArrayList<E>();
+    while (isEmpty() == false) {
+      result.add(poll());
+    }
+    return result;
   }
 }
-  
-
